610677a2d36986ed559b2ef2126ee72dd1a412d9
suprema-swkim <sangwookim@suprema.co.kr>
2024-01-30 14:22:23 +0900

    video component 추가

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_video_time/video_time_lapse.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      builder: (context, child) {
+        return ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: child!,
+        );
+      },
+      home: const SamplePage(),
+    );
+  }
+}
+
+GlobalKey<VideoTimeLapseState> timeLapseKey = GlobalKey();
+DateTime sampleDateTime = DateTime.parse('2023.01.07 13:39:22'.replaceAll('.', '-'));
+
+class SamplePage extends StatelessWidget {
+  const SamplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<bool> timeList = [];
+    for (int i = 0; i < 1440; i++) {
+      timeList.add(Random().nextBool());
+    }
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black54,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                VideoTimeLapse(
+                  timeList: timeList,
+                  focusAndTimeTextBackgroundColor: Colors.green,
+                  timeFocusChanged: (value) {},
+                  previousCallBack: () {},
+                  nextDateCallBack: () {},
+                ),
+                const SizedBox(height: 20),
+                VideoTimeLapse(
+                  timeList: timeList,
+                  focusAndTimeTextBackgroundColor: Colors.blue,
+                  timeFocusChanged: (value) {},
+                  previousCallBack: () {},
+                  nextDateCallBack: () {},
+                ),
+                const SizedBox(height: 20),
+                VideoTimeLapse(
+                  key: timeLapseKey,
+                  timeList: timeList,
+                  timeFocusChanged: (value) {
+                    print('현재 시간 : $value');
+                  },
+                  previousCallBack: () {
+                    print('이전 날짜 콜백');
+                  },
+                  nextDateCallBack: () {
+                    print('다음 날짜 콜백');
+                  },
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(width: 40),
+          FloatingActionButton(
+            heroTag: 'stop',
+            onPressed: () {
+              // stop
+            },
+            child: const Text('stop'),
+          ),
+          const SizedBox(width: 20),
+          FloatingActionButton(
+            heroTag: 'x1',
+            onPressed: () {
+              // 1초당 1번 호출
+              Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+                sampleDateTime = sampleDateTime.add(const Duration(seconds: 1));
+                String formattedDateTime = DateFormat('yyyy.MM.dd HH:mm:ss').format(sampleDateTime);
+                timeLapseKey.currentState!.moveVideoTimeFocus(formattedDateTime);
+              });
+            },
+            child: const Text('x1'),
+          ),
+          const SizedBox(width: 20),
+          FloatingActionButton(
+            heroTag: 'x60',
+            onPressed: () {
+              // 1초당 60번 호출
+              Timer.periodic(const Duration(milliseconds: 16), (timer) {
+                sampleDateTime = sampleDateTime.add(const Duration(seconds: 1));
+                String formattedDateTime = DateFormat('yyyy.MM.dd HH:mm:ss').format(sampleDateTime);
+                timeLapseKey.currentState!.moveVideoTimeFocus(formattedDateTime);
+              });
+            },
+            child: const Text('x60'),
+          ),
+          const SizedBox(width: 20),
+          FloatingActionButton(
+            heroTag: 'x120',
+            onPressed: () {
+              // 1초당 60번 호출
+              Timer.periodic(const Duration(milliseconds: 8), (timer) {
+                sampleDateTime = sampleDateTime.add(const Duration(seconds: 1));
+                String formattedDateTime = DateFormat('yyyy.MM.dd HH:mm:ss').format(sampleDateTime);
+                timeLapseKey.currentState!.moveVideoTimeFocus(formattedDateTime);
+              });
+            },
+            child: const Text('x120'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+}
---
+++ lib/video_time_lapse.dart
@@ -0,0 +1,407 @@
+import 'package:flutter/material.dart';
+
+class VideoTimeLapse extends StatefulWidget {
+  const VideoTimeLapse({
+    Key? key,
+    required this.timeList,
+    required this.timeFocusChanged,
+    required this.previousCallBack,
+    required this.nextDateCallBack,
+    this.timeLineExteriorColor = Colors.grey,
+    this.timeLineColor = Colors.lightBlueAccent,
+    this.timeLineBackgroundColor = Colors.white,
+    this.focusAndTimeTextBackgroundColor = Colors.red,
+    this.timeLineHandColor = Colors.black,
+    this.timeTextColor = Colors.white,
+  }) : super(key: key);
+
+  /// 비디오 시간 목록
+  final List<bool> timeList;
+
+  /// 중심 시간값 변경 시 호출
+  final ValueChanged<String> timeFocusChanged;
+
+  /// 이전 지점 도착 시 호출
+  final VoidCallback previousCallBack;
+
+  /// 다음 지점 도착 시 호출
+  final VoidCallback nextDateCallBack;
+
+  /// 타임 라인 외장 칼라
+  final Color timeLineExteriorColor;
+
+  /// 타임 라인 색상
+  final Color timeLineColor;
+
+  /// 타임 라인 배경 색상
+  final Color timeLineBackgroundColor;
+
+  /// 타임 시간 시간 침 색상
+  final Color timeLineHandColor;
+
+  /// 시간 텍스트 색상
+  final Color timeTextColor;
+
+  /// 표적 및 시간 텍스트 배경 색상
+  final Color focusAndTimeTextBackgroundColor;
+
+  @override
+  State<VideoTimeLapse> createState() => VideoTimeLapseState();
+}
+
+class VideoTimeLapseState extends State<VideoTimeLapse> {
+  /// 비디오 스크롤 컨트롤러
+  late final ScrollController videoScrollController;
+
+  /// 시간 스크롤 컨트롤러
+  late final ScrollController timeScrollController;
+
+  // ------------------------------------------------
+
+  /// 일반 배율
+  late int scale = zoomLevel1Scale;
+
+  /// 실제 배율
+  double get widthSize => scale / 10;
+
+  /// 1시간 기준(30)
+  final int zoomLevel1Scale = 30;
+
+  /// 30분 기준(60)
+  final int zoomLevel2Scale = 60;
+
+  /// 10분 기준(180)
+  final int zoomLevel3Scale = 180;
+
+  // ------------------------------------------------
+
+  /// 최소 줌 스케일
+  int get minScale => zoomLevel1Scale;
+
+  /// 최대 줌 스케일
+  int get maxScale => zoomLevel3Scale;
+
+  bool get isZoomLevel1 => scale < zoomLevel2Scale && scale >= zoomLevel1Scale;
+  bool get isZoomLevel2 => scale < zoomLevel3Scale && scale >= zoomLevel2Scale;
+  bool get isZoomLevel3 => scale == zoomLevel3Scale;
+
+  // ------------------------------------------------
+
+  /// 하루 -> 초로 환산
+  int get dayInSeconds => 86400;
+
+  // ------------------------------------------------
+
+  /// 중심 시간 초 데이터
+  int focusTimeInSeconds = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    videoScrollController = ScrollController();
+    timeScrollController = ScrollController();
+    videoScrollController.addListener(() {
+      /// 스크롤 동기화
+      timeScrollController.jumpTo(videoScrollController.offset);
+
+      // 시작 지점 콜백
+      if (videoScrollController.offset == 0) {
+        widget.previousCallBack();
+        return;
+      }
+
+      // 종료 지점 콜백
+      if (videoScrollController.offset.toInt() == videoScrollController.position.maxScrollExtent.toInt()) {
+        widget.nextDateCallBack();
+        return;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    videoScrollController.dispose();
+    timeScrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: widget.timeLineExteriorColor,
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 80,
+              child: Stack(
+                children: [
+                  GestureDetector(
+                    onScaleStart: (details) {
+                      // 줌 시작 전 위치 저장
+                      focusTimeInSeconds = scrollOffsetToTimeInSeconds();
+                    },
+                    onScaleUpdate: (details) {
+                      /// 핀치줌(가로) 사이즈를 줄일때
+                      if (details.horizontalScale < 1.0 && scale > minScale) {
+                        setState(() {
+                          scale -= 2;
+                        });
+                        return;
+                      }
+
+                      /// 핀치줌(가로) 사이즈를 늘릴때
+                      if (details.horizontalScale > 1.0 && scale < maxScale) {
+                        setState(() {
+                          scale += 2;
+                        });
+                        return;
+                      }
+                    },
+                    onScaleEnd: (details) {
+                      // 줌 시작 전 위치로 이동
+                      double newOffset = timeInSecondsToScrollOffset(focusTimeInSeconds.toDouble());
+                      videoScrollController.animateTo(newOffset, duration: const Duration(milliseconds: 750), curve: Curves.ease);
+                    },
+                    child: NotificationListener<ScrollNotification>(
+                      onNotification: (scrollNotification) {
+                        // 스크롤이 멈췄을때 감지
+                        if (scrollNotification is ScrollEndNotification) {
+                          String hhmmss = formatSecondsToHHMMSS(scrollOffsetToTimeInSeconds());
+                          widget.timeFocusChanged(hhmmss);
+                        }
+                        return true;
+                      },
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        controller: videoScrollController,
+                        padding: EdgeInsets.symmetric(horizontal: (constraints.maxWidth / 2)),
+                        child: Row(
+                          children: List.generate(widget.timeList.length, (index) {
+                            var item = widget.timeList[index];
+                            return Stack(
+                              children: [
+                                Container(
+                                  width: widthSize,
+                                  color: item ? widget.timeLineColor : widget.timeLineBackgroundColor,
+                                ),
+                                if (index % 60 == 0) ...[
+                                  _buildTimeLineHand(30)
+                                ] else if (index % 30 == 0) ...[
+                                  _buildTimeLineHand(20)
+                                ] else if (index % 10 == 0 && (isZoomLevel2 || isZoomLevel3)) ...[
+                                  _buildTimeLineHand(10)
+                                ] else if (index % 2 == 0 && isZoomLevel3) ...[
+                                  _buildTimeLineHand(5)
+                                ]
+                              ],
+                            );
+                          }),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Center(
+                      child: IgnorePointer(
+                        child: SizedBox(
+                          width: 20,
+                          child: CustomPaint(
+                            painter: PentagonPainter(widget.focusAndTimeTextBackgroundColor),
+                            child: const Center(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (isZoomLevel1) ...[
+              SingleChildScrollView(
+                controller: timeScrollController,
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  width: (widget.timeList.length * widthSize) + constraints.maxWidth,
+                  height: 20,
+                  color: widget.focusAndTimeTextBackgroundColor,
+                  child: Stack(
+                    children: List.generate(24, (index) {
+                      final hour = index;
+
+                      return Positioned(
+                        left: (constraints.maxWidth / 2) + (widthSize * 60 * index).toDouble() - 17,
+                        child: Text(
+                          '${hour.toString().padLeft(2, '0')}:00',
+                          style: TextStyle(
+                            color: widget.timeTextColor,
+                            fontSize: 14,
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+            ] else if (isZoomLevel2) ...[
+              SingleChildScrollView(
+                controller: timeScrollController,
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  width: (widget.timeList.length * widthSize) + constraints.maxWidth,
+                  height: 20,
+                  color: widget.focusAndTimeTextBackgroundColor,
+                  child: Stack(
+                    children: List.generate(48, (index) {
+                      final isEvenIndex = index % 2 == 0;
+                      final hour = isEvenIndex ? index : index ~/ 2;
+                      final minute = isEvenIndex ? '00' : '30';
+
+                      return Positioned(
+                        left: (constraints.maxWidth / 2) + (widthSize * 30 * index).toDouble() - 17,
+                        child: Text(
+                          '${hour.toString().padLeft(2, '0')}:$minute',
+                          style: TextStyle(
+                            color: widget.timeTextColor,
+                            fontSize: 14,
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+            ] else if (isZoomLevel3) ...[
+              SingleChildScrollView(
+                controller: timeScrollController,
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  width: (widget.timeList.length * widthSize) + constraints.maxWidth,
+                  height: 20,
+                  color: widget.focusAndTimeTextBackgroundColor,
+                  child: Stack(
+                    children: List.generate(144, (index) {
+                      final hour = index ~/ 6;
+                      final minute = (index % 6) * 10;
+
+                      return Positioned(
+                        left: (constraints.maxWidth / 2) + (widthSize * 10 * index).toDouble() - 17,
+                        child: Text(
+                          '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}',
+                          style: TextStyle(
+                            color: widget.timeTextColor,
+                            fontSize: 14,
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+            ],
+          ],
+        );
+      }),
+    );
+  }
+
+  /// 분침 위젯
+  Widget _buildTimeLineHand(double height) {
+    return Positioned.fill(
+      child: Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          width: 2,
+          height: height,
+          color: widget.timeLineHandColor,
+        ),
+      ),
+    );
+  }
+
+  /// 초(하루) -> 스크롤 위치
+  double timeInSecondsToScrollOffset(double timeInSeconds) {
+    // 시간의 최솟값과 최댓값 정의
+    double minValue = 0.0;
+    double maxValue = videoScrollController.position.maxScrollExtent;
+
+    // 시간의 범위를 구간 수로 나누기
+    double intervalSize = (maxValue - minValue) / dayInSeconds;
+
+    // 주어진 시간을 새 범위로 변환
+    double newValue = timeInSeconds * intervalSize;
+
+    return newValue;
+  }
+
+  /// 스크롤 위치 -> 초(하루)
+  int scrollOffsetToTimeInSeconds() {
+    // 현재 스크롤 위치
+    double currentOffset = videoScrollController.offset;
+
+    // 범위와 구간 수 정의
+    double minValue = 0.0;
+    double maxValue = videoScrollController.position.maxScrollExtent;
+    int intervals = dayInSeconds;
+
+    // 각 구간의 크기 계산
+    double intervalSize = (maxValue - minValue) / intervals;
+
+    // 주어진 값이 몇 번째 구간에 속하는지 계산
+    int intervalIndex = ((currentOffset - minValue) / intervalSize).floor();
+
+    return intervalIndex;
+  }
+
+  /// 초 ->  hh:mm:ss 포맷으로 변환
+  String formatSecondsToHHMMSS(int seconds) {
+    int hours = seconds ~/ 3600;
+    int minutes = (seconds % 3600) ~/ 60;
+    int remainingSeconds = seconds % 60;
+
+    String hoursStr = hours.toString().padLeft(2, '0');
+    String minutesStr = minutes.toString().padLeft(2, '0');
+    String secondsStr = remainingSeconds.toString().padLeft(2, '0');
+
+    return '$hoursStr:$minutesStr:$secondsStr';
+  }
+
+  /// 시간에 맞는 위치로 이동
+  void moveVideoTimeFocus(String dateTimeData) {
+    DateTime dateTime = DateTime.parse(dateTimeData.replaceAll('.', '-'));
+    int seconds = dateTime.hour * 3600 + dateTime.minute * 60 + dateTime.second;
+    double newOffset = timeInSecondsToScrollOffset(seconds.toDouble());
+    videoScrollController.jumpTo(newOffset);
+  }
+}
+
+/// 타입랩스 현재 시점 표시 위젯
+class PentagonPainter extends CustomPainter {
+  const PentagonPainter(this.color);
+
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var width = size.width;
+    var lineWidth = width / 10;
+    var height = size.height;
+
+    var path = Path();
+    path.moveTo((width / 2) - lineWidth, 0);
+    path.lineTo((width / 2) + lineWidth, 0);
+    path.lineTo((width / 2) + lineWidth, height - width);
+    path.lineTo(width, height);
+    path.lineTo(0, height);
+    path.lineTo((width / 2) - lineWidth, height - width);
+    path.close();
+    canvas.drawPath(path, Paint()..color = color);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
